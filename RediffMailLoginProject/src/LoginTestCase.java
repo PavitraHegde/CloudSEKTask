06e79c96d4bc91dd6db5f11c8ed306c9c5a35301
Pavitra hegde <iampavitrahegde@gmail.com>
2020-09-29 12:06:35 +0530

    Login link test case created

--- RediffMailLoginProject/src/LoginTestCase.java
+++ RediffMailLoginProject/src/LoginTestCase.java
@@ -1,8 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTestCase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "//Users//mahabaleshwar//Downloads/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://rediffmail.com");
+		driver.findElement(By.xpath("//*[@id=\'signin_info\']/a[1]")).click();
 
 	}
 
