111c17f538e321827ae84a8bc58272160b63d932
Pavitra hegde <iampavitrahegde@gmail.com>
2020-09-29 12:59:03 +0530

    Added few test cases to create account page

--- RediffMailLoginProject/src/LoginTestCase.java
+++ RediffMailLoginProject/src/LoginTestCase.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LoginTestCase {
 
@@ -9,6 +10,19 @@ public class LoginTestCase {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://rediffmail.com");
 		driver.findElement(By.xpath("//*[@id=\'signin_info\']/a[1]")).click();
+		driver.findElement(By.xpath("//*[@id=\'login1\']")).sendKeys("abc@rediffmail.com");
+		driver.findElement(By.xpath("//*[@id=\'password\']")).sendKeys("abc@123");
+		driver.findElement(By.xpath("//*[@id=\'remember\']")).click();
+		driver.findElement(By.xpath("//input[@name='proceed']")).click();
+        driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/div[2]/form/div[2]/div/a/u")).click();
+        driver.findElement(By.xpath("//*[@id=\'tblcrtac\']/tbody/tr[3]/td[3]/input")).sendKeys("pavitra H");
+        driver.findElement(By.xpath("//*[@id=\'tblcrtac\']/tbody/tr[7]/td[3]/input[1]")).sendKeys("test");
+        driver.findElement(By.xpath("//*[@id=\'tblcrtac\']/tbody/tr[7]/td[3]/input[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\'tblcrtac\']/tbody/tr[24]/td[3]/input[2]")).click();
+        driver.findElement(By.xpath("//*[@id=\'tblcrtac\']/tbody/tr[9]/td[3]/input")).sendKeys("123");
+        driver.findElement(By.xpath("//*[@id=\'tblcrtac\']/tbody/tr[11]/td[3]/input")).sendKeys("123");
+        driver.findElement(By.xpath("//*[@id=\'mobno\']")).sendKeys("12345678");
+      
 
 	}
 
